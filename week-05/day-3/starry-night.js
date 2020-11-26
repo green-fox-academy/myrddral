865fdf79fb0f440b01749469a28a22fc85dff5e6
myrddral <thirdeye0@gmail.com>
2020-11-26 20:59:57 +0100

    starry night implemented

--- week-05/day-3/starry-night.js
+++ week-05/day-3/starry-night.js
@@ -5,6 +5,22 @@ const ctx = canvas.getContext('2d');
 
 // NE NYÚLJ A FENTI KÓDHOZ
 
+ctx.fillStyle = "black";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+function drawStar () {
+    let posX = Math.floor(Math.random() * 600);
+    let posY = Math.floor(Math.random() * 400);
+    ctx.fillStyle = 'grey';
+    ctx.fillRect(posX, posY, 2, 2);
+}
+
+setInterval(function(){ drawStar(); }, 100);
+
+
+
+
+
 // Rajzold ki az éjszakai eget:
 //  - A háttér legyen fekete
 //  - A csillagok kis négyzetek legyenek
