2e3f29dce9e436968aeaac70e9c21ce0b48a3a3f
myrddral <thirdeye0@gmail.com>
2020-11-26 20:10:43 +0100

    functions updated with respective html files to run

---
+++ week-05/day-3/starry-night.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const canvas = document.querySelector('.main-canvas');
+const ctx = canvas.getContext('2d');
+
+// NE NYÚLJ A FENTI KÓDHOZ
+
+// Rajzold ki az éjszakai eget:
+//  - A háttér legyen fekete
+//  - A csillagok kis négyzetek legyenek
+//  - A csillagok legynek véletlenszerüen elhelyezve a canvason
+//  - A csillagok színe változzon véletlenszerűen (szürke árnyalatai)
