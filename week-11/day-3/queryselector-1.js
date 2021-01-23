19054ea15dc75b9ac36fe009adc16fae812a4f46
myrddral <thirdeye0@gmail.com>
2021-01-23 11:36:06 +0100

    queryselector-1 task done

---
+++ week-11/day-3/queryselector-1.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const king = document.getElementById("b325");
+console.log(king);
+const businessLamp = document.querySelectorAll('.big');
+console.log(businessLamp);
+const conceitedKing = 0; // how? 
+const noBusiness = document.querySelectorAll('div.asteroid');
+console.log(noBusiness);
