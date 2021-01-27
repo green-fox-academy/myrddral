2cd458e35f8f68d1365d3ab88135fa4c6448d7a4
myrddral <thirdeye0@gmail.com>
2021-01-27 20:53:27 +0100

    todos.ejs implemented

---
+++ week-12/day-3/todos/todos.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// beállítjuk, hogy view engine (nézet motor) az ejs legyen
+app.set('view engine', 'ejs');
+
+// kezdőoldal
+app.get('/', (req, res) => {
+  // jelenítse meg a `home.ejs`-t
+  res.render('home', {
+    title: 'Things to do today:',
+    todos: ['get up', 'survive', 'go back to bed']
+  });
+});
+
+// a 3000-es porton indítsa el az alkalmazást
+app.listen(PORT, () => {
+  console.log(`Az app a ${PORT}-s port-on fut.`);
+});
