c9ea64422cb83e00644eb59423ffb792946a5591
myrddral <thirdeye0@gmail.com>
2021-01-31 21:56:59 +0100

    added hover zoom to weather widget | fixed greeter's error handling

--- week-12/day-3/frontend/app.js
+++ week-12/day-3/frontend/app.js
@@ -32,11 +32,13 @@ app.get("/greeter", (req, res) => {
     let title = req.query.title;
 
     if (name && title) {
-        res.json({
-            "welcome_message": `Oh, hi there ${name}, my dear ${title}!`
-        })
+        res.json({"welcome_message": `Oh, hi there ${name}, my dear ${title}!`});
+    } else if (!name && title) {
+        res.status(400).json({"error": "Please provide a name!"});
+    } else if (name && !title) {
+        res.status(400).json({"error": "Please provide a title!"});
     } else {
-        res.json({"error": "Please provide name and title!"});
+        res.status(400).json({"error": "Please provide a name and a title!"});
     }
 });
 
@@ -45,8 +47,18 @@ app.get("/appenda/:appendable", (req, res) => {
 
     res.json({
         "appended": `${appendable}a`
-    })
-
-
-
+    });
 });
+
+app.post("/dountil/:action", (req, res) => {
+    let action = req.params.action;
+    
+    res.send('POST message')
+
+    // if (action === "sum") {
+    //     res.send('POST sum')
+    // } else if (action === "factor") {
+    //     res.send('POST factor')
+    // }
+
+})
