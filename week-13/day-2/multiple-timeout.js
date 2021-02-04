313b23d172b9c5eec86ea639fc85ca3f68444cb7
myrddral <thirdeye0@gmail.com>
2021-02-04 22:37:13 +0100

    multiple-timeout implemented

---
+++ week-13/day-2/multiple-timeout.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log('alma');
+setTimeout(() => {
+    console.log('barack');
+}, 1000);
+setTimeout(() => {
+    console.log('dinnye');
+}, 3000);
+setTimeout(() => {
+    console.log('mandarin');
+}, 5000);
