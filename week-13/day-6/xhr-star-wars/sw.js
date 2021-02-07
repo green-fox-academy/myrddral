9237fc2b60d25b1dc33fed68a51e5827695e747c
myrddral <thirdeye0@gmail.com>
2021-02-07 22:34:28 +0100

    xhr starwars lists character searched

---
+++ week-13/day-6/xhr-star-wars/sw.js
@@ -0,0 +1,41 @@
+"use strict";
+
+const input = document.getElementById("input");
+const queryBox = document.getElementById("query-box");
+const button = document.getElementById("search-btn");
+
+button.addEventListener("click", search);
+
+function search() {
+  const httpRequest = new XMLHttpRequest();
+  // httpRequest.onreadystatechange = console.log;
+  httpRequest.open(
+    "GET",
+    `https://swapi.dev/api/people/?search=${input.value}`,
+    true
+  );
+  httpRequest.onload = function () {
+      const response = JSON.parse(this.response);
+      const nameContainer = document.createElement("div");
+
+      nameContainer.innerHTML = response.results[0].name;
+      queryBox.appendChild(nameContainer);
+
+      nameContainer.addEventListener("click", listFilms);
+
+      function listFilms() {
+        let films = response.results[0].films;
+
+        for (let i = 0; i < films.length; i++) {
+          const httpRequest = new XMLHttpRequest();
+          httpRequest.open("GET", `${films[i]}`, true);
+          const response = JSON.parse(this.response);
+          const filmContainer = document.createElement("div");
+          console.log(response.results);
+          filmContainer.innerHTML = response.results[0].title;
+          queryBox.appendChild(filmContainer);
+        }
+    }
+  };
+  httpRequest.send(null);
+}
