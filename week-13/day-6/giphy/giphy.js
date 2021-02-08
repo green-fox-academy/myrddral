2b79ba21a2b308ee66dc2634065cb368cd8475bc
myrddral <thirdeye0@gmail.com>
2021-02-08 23:45:12 +0100

    giphy app displays 16 static thumbnails for search term

--- week-13/day-6/giphy/giphy.js
+++ week-13/day-6/giphy/giphy.js
@@ -1,8 +1,15 @@
 "use strict";
 
+const contentBox = document.getElementById("content-box");
 const button = document.getElementById("searchBtn");
 const input = document.getElementById("input");
 button.addEventListener("click", search);
+input.addEventListener("keyup", function (event) {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    document.getElementById("searchBtn").click();
+  }
+});
 
 function search() {
   const httpRequest = new XMLHttpRequest();
@@ -11,13 +18,21 @@ function search() {
     `http://api.giphy.com/v1/gifs/search?q=${input.value}&api_key=xM86VSEX3dygGWMhlXtntXi3OGoA6vuZ`,
     true
   );
-//   httpRequest.setRequestHeader(
-//     "Authorization",
-//     "xM86VSEX3dygGWMhlXtntXi3OGoA6vuZ"
-//   );
   httpRequest.send(null);
 
   httpRequest.onload = function () {
     const response = JSON.parse(this.response);
+    
+    for (let i = 0; i < 16; i++) {
+      insertThumbnail(response.data[i].images.downsized_still);
+    }
+
+    function insertThumbnail(thumbnail) {
+      const newThumbnail = document.createElement("IMG");
+      newThumbnail.setAttribute("src", `${thumbnail.url}`);
+      newThumbnail.setAttribute("width", "100px");
+      newThumbnail.setAttribute("height", "auto");
+      contentBox.appendChild(newThumbnail);
+    }
   };
 }
