20d8c77c8a6d4f61283415ebb53c4b0b41c19bc1
myrddral <thirdeye0@gmail.com>
2021-02-09 18:51:24 +0100

    xhr starwars code cleanup

--- week-13/day-6/xhr-star-wars/sw.js
+++ week-13/day-6/xhr-star-wars/sw.js
@@ -15,26 +15,28 @@ function search() {
     true
   );
   httpRequest.onload = function () {
-      const response = JSON.parse(this.response);
-      const nameContainer = document.createElement("div");
+    const response = JSON.parse(this.response);
+    const nameContainer = document.createElement("div");
 
-      nameContainer.innerHTML = response.results[0].name;
-      queryBox.appendChild(nameContainer);
+    nameContainer.innerHTML = response.results[0].name;
+    queryBox.appendChild(nameContainer);
 
-      nameContainer.addEventListener("click", listFilms);
+    nameContainer.addEventListener("click", listFilms);
 
-      function listFilms() {
-        let films = response.results[0].films;
-
-        for (let i = 0; i < films.length; i++) {
-          const httpRequest = new XMLHttpRequest();
-          httpRequest.open("GET", `${films[i]}`, true);
+    function listFilms() {
+      let films = response.results[0].films;
+      //   console.log(`${films[0]}`);
+      for (let i = 0; i < films.length; i++) {
+        const httpRequest = new XMLHttpRequest();
+        httpRequest.open("GET", `${films[i]}`, true);
+        httpRequest.onload = function () {
           const response = JSON.parse(this.response);
           const filmContainer = document.createElement("div");
-          console.log(response.results);
           filmContainer.innerHTML = response.results[0].title;
           queryBox.appendChild(filmContainer);
-        }
+          httpRequest.send(null);
+        };
+      }
     }
   };
   httpRequest.send(null);
