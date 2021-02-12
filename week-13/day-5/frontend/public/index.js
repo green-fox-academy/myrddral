aff3b296d46d01bafe65b63c2ec8be5a2e36aa68
myrddral <thirdeye0@gmail.com>
2021-02-12 15:07:09 +0100

    chat app updated with firestore DB

--- week-13/day-5/frontend/public/index.js
+++ week-13/day-5/frontend/public/index.js
@@ -1,3 +1,16 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCO-pldxfyp6f0-Q5L7YBAit-TunjZ0CtM",
+    authDomain: "seo-chat-b988e.firebaseapp.com",
+    projectId: "seo-chat-b988e",
+    storageBucket: "seo-chat-b988e.appspot.com",
+    messagingSenderId: "581799017478",
+    appId: "1:581799017478:web:d374b12f6bf0bacf245787"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+const messagesCollection = db.collection('messages');
 const apiUrl = 'http://80.98.81.178:3000';
 
 function getMessages() {
@@ -31,7 +44,46 @@ function getMessages() {
         });
 }
 
-getMessages();
+function getDBMessages() {
+    messagesCollection
+        .orderBy('datetime', 'asc')
+        .get()
+        .then(processDBMessages)
+        .then(updateScroll)
+        .catch((error) => {
+            console.error(error.message);
+        });
+}
+
+function processDBMessages(messages) {
+    const tbody = document.getElementById('all-messages');
+    tbody.innerHTML = '';
+
+    messages.forEach((doc) => {
+        const message = doc.data();
+        const tr = document.createElement('tr');
+        const tdDatetime = document.createElement('td');
+        tdDatetime.classname = "datetime";
+        const tdSender = document.createElement('td');
+        tdSender.classname = "sender";
+        const tdMessage = document.createElement('td');
+        tdMessage.classname = "message";
+
+        const datetime = new Date(message.datetime.seconds * 1000);
+
+        tdDatetime.textContent = `${ datetime.getFullYear() }.${ datetime.getMonth() + 1 }.${ datetime.getDate() }. ${ datetime.getHours() }:${ datetime.getMinutes() }`;
+        tdSender.textContent = message.sender;
+        tdMessage.textContent = message.message;
+
+        tr.appendChild(tdDatetime);
+        tr.appendChild(tdSender);
+        tr.appendChild(tdMessage);
+
+        tbody.prepend(tr);
+    });
+}
+
+getDBMessages();
 
 function sendMessage() {
     const data = {
@@ -55,5 +107,32 @@ function sendMessage() {
     });
 }
 
-document.getElementById('send-btn').addEventListener('click', sendMessage);
-document.getElementById('update-btn').addEventListener('click', getMessages);
+function sendDBMessage() {
+    const data = {
+        sender: document.getElementById('name').value,
+        message: document.getElementById('message').value,
+        datetime: new Date()
+    };
+
+    messagesCollection
+        .add(data)
+        .then((doc) => {
+            console.log(`CREATE OK: ${ doc.id }`);
+            document.getElementById('message').value = '';
+        })
+        .catch((error) => {
+            console.error(error.message);
+        });
+}
+
+function updateScroll(){
+    var element = document.querySelector('messages-box')
+    element.scrollTop = element.scrollHeight;
+}
+
+document.getElementById('send-btn').addEventListener('click', sendDBMessage);
+// document.getElementById('update-btn').addEventListener('click', getDBMessages);
+
+messagesCollection
+    .orderBy('datetime', 'desc')
+    .onSnapshot(processDBMessages);
