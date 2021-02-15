3b782102dbe55772a8fdec91fea97e6783ca3773
myrddral <thirdeye0@gmail.com>
2021-02-15 09:19:54 +0100

    notebook error cleanup

--- hsg-third-exam-practice/green-fox-notebook-error/backend/public/js/index.js
+++ hsg-third-exam-practice/green-fox-notebook-error/backend/public/js/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
 const reporter = document.getElementById('reporter');
-// const jsonContent = fs.readFileSync("data.json", "utf-8");
-// const database = JSON.parse(jsonContent);
+
+fetch('http://127.0.0.1:3000/users')
+  .then(response => response.json())
+  .then(data => console.log(data));
 
 const option = document.createElement('option');
 reporter.appendChild(option);
