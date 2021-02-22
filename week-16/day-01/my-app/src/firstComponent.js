f8ea0d3a1aecb93cfb841587b66e9c793713213a
Attila Beli <ikoza@Attilas-MacBook-Pro.local>
2021-02-22 15:07:25 +0100

    react intro - first components

---
+++ week-16/day-01/my-app/src/firstComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function FirstComponent() {
+  return <p>Üdv az első komponensemből!</p>;
+}
---
+++ week-16/day-01/my-app/src/secondComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SecondComponent(props) {
+  return <p>Üdv az második komponensemből!</p>;
+}
