a318540d61dc35424a04a18aa75e26ffefb5d63b
Attila Beli <thirdeye0@gmail.com>
2021-02-25 22:31:24 +0100

    fetch introduced to handle blog post requests

--- week-16/day-02/dojo-blog/src/BlogList.js
+++ week-16/day-02/dojo-blog/src/BlogList.js
@@ -1,17 +1,15 @@
-const BlogList = ({blogs, title, handleDelete}) => {
-
-    return (
-        <div className="blog-list">
-            <h1>{title}</h1>
-            {blogs.map((blog) => (
-             <div className="blog-preview" key={blog.id}>
-                 <h2>{blog.title}</h2>
-                 <p>Written by {blog.author}</p>
-                 <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
-             </div>
-         ))}
+const BlogList = ({ blogs, title }) => {
+  return (
+    <div className="blog-list">
+      <h1>{title}</h1>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
         </div>
-    );
-}
- 
+      ))}
+    </div>
+  );
+};
+
 export default BlogList;
