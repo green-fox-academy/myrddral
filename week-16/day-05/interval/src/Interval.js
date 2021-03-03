29472da03455fa995358268faeccef7a65e620a1
Attila Beli <thirdeye0@gmail.com>
2021-03-03 16:27:32 +0100

    interval implemented

--- week-16/day-05/interval/src/Interval.js
+++ week-16/day-05/interval/src/Interval.js
@@ -1,12 +1,27 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const Interval = () => {
-    const [counter, setCounter] = useState(0);
+  
+  let [counter, setCounter] = useState(0);
+  let [increment, setIncrement] = useState(1000)
+
+  const clickHandler = () => {
+      setIncrement(500)
+  }
+  
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log(increment);
+      setCounter(counter++)
+        }, increment);
+        return () => clearInterval(interval);
+      }, [increment]);
 
 
     return ( 
         <div>
             <h2>Interval counter</h2>
+            <button onClick={clickHandler}>Click me</button>
             <div className="counter">{counter}</div>
         </div>
      );
